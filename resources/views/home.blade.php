fea9febbbbf2ac15b78e0741b3738dd19feb5b40
DESKTOP-H7K2FLP\aokim <aokimakoto0322@gmail.com>
2021-09-25 07:25:21 +0900

    bootstrap5対応

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,21 +15,24 @@
     <body>
 
         {{-- ライバー紹介エリア --}}
-        
         <div class="container mx-auto mt-5">
             <h1 class="display-5 my-4 mx-2">ライバー</h1>
-            <div class="row">
-                <div class="col-md me-md-3 pt-3 px-3 pt-md-5 px-md-5 d-flex align-items-center">
-                    <div class="row">
-                        <h2 class="display-6">理念</h2>
-                        <p>ライバーの理念とかどういう目標があって活動してますとかいろいろやってます的なテキスト</p>
+            <div class="card shadow-lg">
+                <div class="row">
+                    <div class="col-md-5 mx-auto d-flex align-items-center">
+                        <div class="row">
+                            <h2 class="display-6">理念</h2>
+                            <p>ライバーの理念とかどういう目標があって活動してますとかいろいろやってます的なテキスト</p>
+                            <p>ライバーの理念とかどういう目標があって活動してますとかいろいろやってます的なテキスト</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md mx-auto my-2">
-                    <img src="/img/k1.jpg" width="auto" height="800px">
+                    <div class="col-md-5 mx-auto my-2">
+                        <img src="/img/k1.jpg" width="auto" height="800px">
+                    </div>
                 </div>
             </div>
         </div>
+        
         {{--　トピックエリア  --}}
         @isset($news)
             <div class="container mx-auto mt-5">
@@ -44,10 +47,13 @@
                                     <a href="{{ $item->url }}">
                                 @endif
                                     <img width="100%" height="300" src="{{ $item->imageUrl ?? '' }}">
+
+                                    {{-- サブジェクト --}}
                                     <div class="card-body">
-                                        <h4>{{ $item->topic ?? 'a' }}</h4>
+                                        <h2 class="lead">{{ $item->topic ?? 'a' }}</h2>
                                     </div>
 
+                                    {{-- トピックスボディ --}}
                                     <div class="card-text mx-4 my-4">
                                         {{ $item->detail }}
                                     </div>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,25 +1,25 @@
 @section('header')
     <header>
-        <nav class="navbar navbar-light navbar-expand-md bg-light">
+        <nav class="navbar navbar-dark navbar-expand-md bg-dark">
             <a class="navbar-brand" href="/">
-                <img src="/img/logo.png" class="logo-img" width="200px" height="auto">
+                <img src="/img/logo.png" class="logo-img mx-4" width="350px" height="auto">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarmenu" aria-controls="navbarmenu" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarmenu" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarmenu">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="p-3 text-dark" href="/">Home</a>
+                    <li class="nav-item py-3 mx-2">
+                        <a class="p-3 text-white" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="p-3 text-dark" href="/news">ニュース</a> 
+                    <li class="nav-item py-3 mx-2">
+                        <a class="p-3 text-white" href="/news">ニュース</a> 
                     </li>
-                    <li class="nav-item">
-                        <a class="p-3 text-dark" href="./introduction">メンバー</a> 
+                    <li class="nav-item py-3 mx-2">
+                        <a class="p-3 text-white" href="./introduction">メンバー</a> 
                     </li>
-                    <li class="nav-item">
-                        <a class="p-3 text-dark" href="./contact">お問い合わせ</a> 
+                    <li class="nav-item py-3 mx-2">
+                        <a class="p-3 text-white" href="./contact">お問い合わせ</a> 
                     </li>
                 </ul>
             </div>
