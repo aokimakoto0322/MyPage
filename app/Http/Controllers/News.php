51779808df0446e506ed5ad9d3e2a63e0d9266e7
DESKTOP-H7K2FLP\aokim <aokimakoto0322@gmail.com>
2021-09-29 21:47:28 +0900

    ニュース画面完成

--- app/Http/Controllers/News.php
+++ app/Http/Controllers/News.php
@@ -5,6 +5,18 @@ namespace App\Http\Controllers;
 class News extends Controller
 {
     public function index(){
-        return view('news');
+        //タイムゾーン設定
+        date_default_timezone_set('Asia/Tokyo');
+
+        //日付取得
+        $today = date("Y/m/d h:i:s");
+
+        //newsトピックの取得
+        $topics = \DB::table('news')->where('enableflag', '=', 1)->where('startdate' , '<', $today)->where('enddate' , '>', $today)->orderByDesc('id')->get();
+
+        //bladeへ送るデータ作成
+        $result = ['news' => $topics];
+
+        return view('news', $result);
     }
 }
