b75a4adf621159017983b7b74a7f34f4ebcb7685
青木誠 <aokimakoto@aokimakotonoMacBook-Air.local>
2021-10-17 19:52:22 +0900

    セッションDB保存

--- resources/views/adminlayout/adminhead.blade.php
+++ resources/views/adminlayout/adminhead.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <title>@yield('Pagetitle')</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="robots" content="noindex">
+    <meta name="robots" content="noindex,noarchive">
     <meta name="googlebot" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/slick.css') }}" media="screen" />
